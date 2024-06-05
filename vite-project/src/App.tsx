fac5c936d9df4d65fdb381625d6e8514e3f54500
Yuamy205 <ngocarmy0710g@gmail.com>
2024-06-05 07:39:25 +0700

    240605

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,18 @@
+import Bai1 from "./ss26/Bai1"
+import Bai2 from "./ss26/Bai2"
+import Bai3 from "./ss26/Bai3"
+
+
+function App() {
+
+
+  return (
+    <>
+      <Bai1/>
+      <Bai2/>
+      <Bai3/>
+    </>
+  )
+}
+
+export default App
